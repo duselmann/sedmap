fe9d252b98581c9e9921b9ef117d441865d0df80
David Uselmann <duselmann@usgs.gov>
2013-07-23 13:05:16 -0500

    NSM-40,13,27 and others -- added siteinfo view, and new columns with test data

---
+++ liquibase_files/changelog1SiteRefNewColumns.sql
@@ -0,0 +1,26 @@
+--liquibase formatted sql
+
+--This is for the sedmap schema
+ 
+
+--changeset duselman:AddToSiteInfoBasinLandK
+alter table sm_site_ref add (
+ GAGE_BASIN_ID varchar2(15),
+ PCT_URBAN  number(2,0),
+ PCT_AG     number(2,0),
+ PCT_FOREST number(2,0),
+ SOIL_K     number(3,3)
+-- more possible k values
+-- SOIL_K_PCT number(6,3),
+-- SOIL_K_UP  number(6,3),
+-- SOIL_K_PCT number(3,3)
+ );
+
+-- populate with test data
+--changeset duselman:AddToSiteInfoBasinLandKSampleData
+update sm_site_ref set
+GAGE_BASIN_ID = trunc(DBMS_RANDOM.value(low => 1, high => 9)*100000000000000),
+PCT_URBAN  = trunc(DBMS_RANDOM.value(low => 0, high => 10)*10),
+PCT_AG     = trunc(DBMS_RANDOM.value(low => 0, high => 10)*10),
+PCT_FOREST = trunc(DBMS_RANDOM.value(low => 0, high => 10)*10),
+SOIL_K     = trunc(DBMS_RANDOM.value(low => 0, high => 1)*1000)/1000;
--- liquibase_files/changelog1Views.sql
+++ liquibase_files/changelog1Views.sql
@@ -36,3 +36,30 @@ left outer join SM_REFERENCE_SITES r
 where exists (select 1 
 			from sedmap.sm_daily_sites i 
 			where s.usgs_station_id=i.usgs_station_id)
+
+			
+			
+--changeset duselman:CreateSiteInfoView
+create or replace view sedmap.SM_SITE_INFO as
+select s.USGS_STATION_ID,STATION_NAME,LATITUDE,LONGITUDE,GEOM_LL,HUC_12,DA,"STATE",
+    y.minyr as daily_min, y.maxyr as daily_max, y.minyr ||'-'|| y.maxyr as Daily_Period,
+    f.minyr as discrete_min, f.maxyr as discrete_max, f.minyr ||'-'|| f.maxyr as Discrete_Period,
+    NVL2(r.USGS_STATION_ID, '1','0') as REFERENCE_SITE,
+      NVL( (select 1 from sedmap.sm_daily_sites d where s.usgs_station_id=d.usgs_station_id) ,0) as daily_site,
+      NVL( (select 1 from sedmap.sm_inst_sites  d where s.usgs_station_id=d.usgs_station_id) ,0) as discrete_site,
+      NVL(y.sample_years,0) as daily_years,
+      NVL(f.discrete_samples,0) as discrete_samples
+  from sedmap.sm_site_ref s 
+  left outer join SM_REFERENCE_SITES r on s.USGS_STATION_ID = r.USGS_STATION_ID
+  left outer join (
+    select usgs_station_id, count(*) sample_years, min(sample_year) minyr, max(sample_year) maxyr 
+      from sm_daily_year y 
+     group by usgs_station_id) y 
+  on (y.usgs_station_id = s.usgs_station_id)
+  left outer join (
+    select usgs_station_id, count(*) discrete_samples, EXTRACT(year FROM min(datetime)) minyr, EXTRACT(year FROM max(datetime)) maxyr 
+      from sm_inst_sample_fact f 
+     group by usgs_station_id) f 
+  on (f.usgs_station_id = s.usgs_station_id)
+;
+			
