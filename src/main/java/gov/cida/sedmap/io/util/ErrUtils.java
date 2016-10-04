a1da2aeff9c57efccba6342622c71147eebdc125
David Uselmann <duselmann@flexion.us>
2016-10-04 17:51:25 -0500

    remove stubborn javadox br tags

--- src/main/java/gov/cida/sedmap/io/util/ErrUtils.java
+++ src/main/java/gov/cida/sedmap/io/util/ErrUtils.java
@@ -43,13 +43,10 @@ public class ErrUtils {
 	 * @param res
 	 * @param e
 	 * @return
-	 * <br /><br />
 	 * This method will push a standard OGC error response back to the client dictating what
 	 * the issue was.
-	 * <br /><br />
 	 * OGC Error Messages taken from: http://www.ogcnetwork.net/node/198
-	 * <br /><br />
-	 * An example of an OGC XML error is:<br /><br />
+	 * An example of an OGC XML error is:
 	 * <pre>
 	 * {@code
 	 * <?xml version="1.0" encoding="UTF-8"?>
@@ -66,7 +63,6 @@ public class ErrUtils {
 	 * 
 	 * <table border="1" cellpadding="8" cellspacing="1"><tbody><tr><td width="153" valign="top"><p align="center"><strong>exceptionCode value </strong></p></td><td width="149" valign="top"><p align="center"><strong>Meaning of code </strong></p></td><td width="149" valign="top"><p align="center"><strong>“locator” value </strong></p></td></tr><tr><td width="153" valign="top"><p>OperationNotSupported </p></td><td width="149" valign="top"><p>Request is for an operation that is not supported by this server </p></td><td width="149" valign="top"><p>Name of operation not supported </p></td></tr><tr><td width="153" valign="top"><p>MissingParameterValue </p></td><td width="149" valign="top"><p>Operation request does not include a parameter value, and this server did not declare a default value for that parameter </p></td><td width="149" valign="top"><p>Name of missing parameter </p></td></tr><tr><td width="153" valign="top"><p>InvalidParameterValue </p></td><td width="149" valign="top"><p>Operation request contains an invalid parameter value a </p></td><td width="149" valign="top"><p>Name of parameter with invalid value </p></td></tr><tr><td width="153" valign="top"><p>ResourceNotFound</p></td><td width="149" valign="top"><p>This is a configuration error. This could be a plugin not found in a java environment where the classloader misses the class in the CLASSPATH or a required interface in a class is not implemented.</p></td><td width="149" valign="top"><p>A short description what went wrong (i.e. the name of the faulty or required class).</p></td></tr><tr><td width="153" valign="top"><p>NoApplicableCode </p></td><td width="149" valign="top"><p>No other exceptionCode specified by this service and server applies to this exception </p></td><td width="149" valign="top"><p>None, omit “locator” parameter </p></td></tr><tr><td width="451" colspan="3" valign="top"><p>a When an invalid parameter value is received, it seems desirable to place the invalid value(s) in ExceptionText string(s) associated with the InvalidParameterValue value. </p></td></tr></tbody></table>
 	 * </pre>
-	 * <br /><br />
 	 * 	Exception Codes:
 	 * 		OperationNotSupported
 	 * 		MissingParameterValue
@@ -74,7 +70,6 @@ public class ErrUtils {
 	 * 		ResourceNotFound
 	 * 		NoApplicableCode
 	 * 
-	 * <br /><br />
 	 * Its probably smart not to show a stack trace w/ class names and line numbers so
 	 * in the <ExceptionText> element we will just put the exception message
 	 * 		
