e17e053d91ccb4b68abc34b250492439de91602d
David Uselmann <duselmann@usgs.gov>
2016-10-17 10:07:30 -0500

    Revert "test bypass tomcat connection DS to test if the pool is causing connection reset issues"
    This reverts commit 56a289c83f8a9edd307d3c57d5427e4ee84e90d9.

--- src/main/java/gov/cida/sedmap/data/JdbcFetcher.java
+++ src/main/java/gov/cida/sedmap/data/JdbcFetcher.java
@@ -14,7 +14,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -339,14 +338,8 @@ public class JdbcFetcher extends Fetcher {
 
 		try {
 			Context ctx = getContext();
-//			DataSource ds = (DataSource) ctx.lookup(jndiDS);
-//			r.cn = ds.getConnection();
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			String url = (String) ctx.lookup("java:comp/env/jdbc/sedmapURL");
-			String usr = (String) ctx.lookup("java:comp/env/jdbc/sedmapUSR");
-			String pwd = (String) ctx.lookup("java:comp/env/jdbc/sedmapPWD");
-			r.cn = DriverManager.getConnection(url, usr, pwd);
-			
+			DataSource ds = (DataSource) ctx.lookup(jndiDS);
+			r.cn = ds.getConnection();
 		} catch (SQLException e) {
 			throw new SedmapException(OGCExceptionCode.ResourceNotFound, e);
 		}
